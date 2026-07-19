3973dc092bc02179dc861d9320a4b10d19dab642
Venkata jagadeesh Mandava <venkatajagadeeshmandava@Venkatas-MacBook-Pro.local>
2026-07-19 02:18:45 -0400

    Raise JSON body limit to allow profile photo uploads

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const twilio = require("twilio");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(app.use(express.json({ limit: "8mb" }));express.json());
 
 const {
   TWILIO_ACCOUNT_SID,
